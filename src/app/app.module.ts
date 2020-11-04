073a7bfe93ef03413d4c193d7a6c7266404d02e5
ChavaMS <chava678ics@gmail.com>
2020-11-03 19:43:00 -0600

    Avance de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+
+//Servicios
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -14,10 +19,13 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    appRoutingProviders
+    appRoutingProviders,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public title: String;
+  constructor() {
+    this.title = "Registro";
+   }
 
   ngOnInit(): void {
+
   }
 
 }
