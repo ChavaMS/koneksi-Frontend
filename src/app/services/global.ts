c06e4664a90fb9134398c95c2c5950b79049af26
ChavaMS <chava678ics@gmail.com>
2020-12-12 18:33:06 -0600

    Global fix

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-    //url: 'https://afternoon-sierra-90930.herokuapp.com/api/'
-    url: 'http://localhost:3000/api/'
+    url: 'https://afternoon-sierra-90930.herokuapp.com/api/'
+    //url: 'http://localhost:3000/api/'
 };
